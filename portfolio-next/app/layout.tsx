261d2e4f724b77697849456f27ef101d0d279dc3
Bandi86 <susutechno@gmail.com>
2023-05-12 13:34:30 +0200

    layout fix

--- portfolio-next/app/layout.tsx
+++ portfolio-next/app/layout.tsx
@@ -2,8 +2,6 @@ import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-
-
 export const metadata = {
 	title: 'My Portfolio', // update this later
 	description: '',
@@ -11,8 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html
-			lang='en'>
+		<html lang='en'>
 			<body>
 				<Navbar />
 				{children}
